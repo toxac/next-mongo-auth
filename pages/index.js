677588ee0327111ca2039b85f78ca1470eca05dd
toxac <derocon@gmail.com>
2023-04-18 23:10:46 +0530

    handleEvents

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,17 @@ import GithubIcon from '../assets/github.svg'
 export default function Home() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+
+  const handleSignInWithEmail = async()=>{
+    alert(JSON.stringify({email, password}, 2, null))
+  }
+  const handleSignInWithGoogle =async()=>{
+    alert("Google")
+  }
+  const handleSignInWithGithub = async()=>{
+    alert("Github")
+  }
+
   return (
     <div>
       <Head>
@@ -22,7 +33,7 @@ export default function Home() {
         <div className="flex flex-col w-1/4 space-y-2 mb-6">
           <input 
             type="text" 
-            className="h-8 p-3 border-2 border-slate-700 rounded-md mb-2 " 
+            className="h-8 p-3 border-2 border-slate-700 rounded-md mb-1 " 
             name="email" 
             value={email} 
             onChange={(e)=> setEmail(e.target.value)} 
@@ -36,16 +47,27 @@ export default function Home() {
             onChange={(e)=> setPassword(e.target.value)} 
             placeholder='Password' 
           />
+          <div className="bg-black  rounded-md hover:rounded-full px-4 py-2 text-center drop-shadow-lg flex justify-center items-center space-x-2 mb-2">
+              <Image src={GoogleIcon} className='w-5 h-5 text-white'/>
+              <button className="text-sm font-semibold text-slate-200 hover:text-slate-50" onClick={handleSignInWithEmail}>
+                SIGNIN WITH EMAIL
+              </button>
+          </div>
+          <hr/>
         </div>
 
         <div className="flex flex-col w-1/4 space-y-2">
           <div className="bg-black  rounded-md hover:rounded-full px-4 py-2 text-center drop-shadow-lg flex justify-center items-center space-x-2">
               <Image src={GoogleIcon} className='w-5 h-5 text-white'/>
-              <p className="text-sm font-semibold text-slate-200 hover:text-slate-50">SIGNIN WITH GOOGLE</p>
+              <button className="text-sm font-semibold text-slate-200 hover:text-slate-50" onClick={handleSignInWithGoogle}>
+                SIGNIN WITH GOOGLE
+              </button>
           </div>
           <div className="bg-black  rounded-md hover:rounded-full px-4 py-2 text-center drop-shadow-lg flex justify-center items-center space-x-2">
             <Image src={GithubIcon} className='w-5 h-5 text-white'/>
-            <p className="text-sm font-semibold text-slate-200 hover:text-slate-50">SIGNIN WITH GITHUB</p>
+            <button className="text-sm font-semibold text-slate-200 hover:text-slate-50" onClick={handleSignInWithGithub}>
+              SIGNIN WITH GITHUB
+            </button>
           </div>
         </div>
       </div>
